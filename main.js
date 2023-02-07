5ff0d9d99f00b1bab120d5a19afd7cef50a9e0e1
Piotr Konieczny <piotrekprogramista@gmail.com>
2023-02-07 23:12:29 +0100

    adding an about us section

--- main.js
+++ main.js
@@ -1,30 +1,29 @@
-let navbar = document.querySelector('.navbar');
+const navbar = document.querySelector('.navbar');
 
-document.querySelector('#menu-btn').onclick = () =>{
-    navbar.classList.toggle('active');
-    searchForm.classList.remove('active')
-    cartItem.classList.remove('active')
-} 
+document.querySelector('#menu-btn').onclick = () => {
+  navbar.classList.toggle('active');
+  searchForm.classList.remove('active');
+  cartItem.classList.remove('active');
+};
 
-let searchForm = document.querySelector('.search-form');
+const searchForm = document.querySelector('.search-form');
 
-document.querySelector('#search-btn').onclick = () =>{
-    searchForm.classList.toggle('active');
-    navbar.classList.remove('active')
-    cartItem.classList.remove('active')
-} 
+document.querySelector('#search-btn').onclick = () => {
+  searchForm.classList.toggle('active');
+  navbar.classList.remove('active');
+  cartItem.classList.remove('active');
+};
 
-let cartItem = document.querySelector('.cart-items-container');
+const cartItem = document.querySelector('.cart-items-container');
 
-document.querySelector('#cart-btn').onclick = () =>{
-    cartItem.classList.toggle('active');
-    navbar.classList.remove('active')
-    searchForm.classList.remove('active')
-    
-}
+document.querySelector('#cart-btn').onclick = () => {
+  cartItem.classList.toggle('active');
+  navbar.classList.remove('active');
+  searchForm.classList.remove('active');
+};
 
-window.onscroll = () =>{
-    navbar.classList.remove('active')
-    searchForm.classList.remove('active')
-    cartItem.classList.remove('active')
-}
+window.onscroll = () => {
+  navbar.classList.remove('active');
+  searchForm.classList.remove('active');
+  cartItem.classList.remove('active');
+};
