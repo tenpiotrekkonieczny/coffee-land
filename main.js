532862bef424af15382376fe9b25ad4c5cc40cc6
Piotr Konieczny <piotrekprogramista@gmail.com>
2023-02-13 21:39:52 +0100

    adding a footer section

--- main.js
+++ main.js
@@ -27,3 +27,13 @@ window.onscroll = () => {
   searchForm.classList.remove('active');
   cartItem.classList.remove('active');
 };
+
+
+const footerYear = document.querySelector('.footer__year');
+
+const handleCurrentYear = () => {
+  const year = (new Date()).getFullYear();
+  footerYear.innerText = year;
+};
+
+handleCurrentYear();
